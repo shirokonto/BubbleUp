1c7522737e29514b9f32383ba159ef6d6459d326
Shiro Skrattberg <hikari.hanakimi@gmail.com>
2022-06-02 23:47:39 +0200

    Add simple waypoint path system and info objects

---
+++ BubbleUp/Assets/Features/Interactables_Namespace/Scripts/Waypoints.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    [Range(0f, 0.5f)]
+    [SerializeField] private float wayPointSize = 1f;
+    private void OnDrawGizmos()
+    {
+        foreach (Transform t in transform)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(t.position, wayPointSize); //take every transform for every waypoints
+        }
+
+        Gizmos.color = Color.red;
+        for (int i = 0; i < transform.childCount - 1; i++)
+        {
+            Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild(i+1).position);
+        }
+    }
+
+    //TODO: adapt for our two nodes only path
+    public Transform GetNextWayPoint(Transform currentWayPoint)
+    {
+        if (currentWayPoint == null)
+        {
+            return transform.GetChild(0);
+        }
+
+        if (currentWayPoint.GetSiblingIndex() < transform.childCount - 1)
+        {
+            return transform.GetChild(currentWayPoint.GetSiblingIndex() + 1); //get next point
+        }
+        else
+        {
+            return transform.GetChild(0);
+        }
+    }
+}
