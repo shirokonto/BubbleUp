08a2720edf6a68f086682f2d0bb6cf2caf936399
Shiro Skrattberg <hikari.hanakimi@gmail.com>
2022-06-05 15:47:01 +0200

    Add maximize bubble variable

--- BubbleUp/Assets/Features/Bubble_Namespace/Scripts/BubbleBehaviour.cs
+++ BubbleUp/Assets/Features/Bubble_Namespace/Scripts/BubbleBehaviour.cs
@@ -1,16 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class BubbleBehaviour : MonoBehaviour
 {
     [SerializeField] private string correctInfoType; //will be set before the game starts via character view
     private int _temporaryCount;
+    private float _maximizeBubble = -0.03f;
+
+    private int _wrongItemCounter = 3;
+    //add minimizebubble float for minimizeBubbleitem
+    private Vector3 _scaleChange;
 
     private void Awake()
     {
         _temporaryCount = 0;
+        _scaleChange = new Vector3(_maximizeBubble, _maximizeBubble, _maximizeBubble);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -25,6 +33,11 @@ public class BubbleBehaviour : MonoBehaviour
             else
             {
                 _temporaryCount -= 1;
+                transform.localScale -= _scaleChange;
+                //_wrongItemCounter.Equals(0) ? Debug.Log("GAME OVER") : _wrongItemCounter -=1;
+                //var result = x > y ? "x is greater than y" : "x is less than y";
+                //Console.WriteLine((_wrongItemCounter == 0) ? _wrongItemCounter true : false);
+
                 Debug.Log("( - ) TemporaryCount: " + _temporaryCount + " :-( ");
             }
             collision.gameObject.SetActive(false);
