49e1f183bff9f6ed466e03595eb85028dc437c0d
Shiro Skrattberg <hikari.hanakimi@gmail.com>
2022-06-22 20:36:28 +0200

    Revert "Merge pull request #18 from shirokonto/feature/#011_AddCharacterAnimationAfterBubbleBurst"
    This reverts commit cdb2c57e55bb783160b76f3edfaf92983ee9c923, reversing
    changes made to de29f039c72bb6e3c5b86226bc58a2150905974e.

--- BubbleUp/Assets/Features/Bubble_Namespace/Scripts/BubbleBehaviour.cs
+++ BubbleUp/Assets/Features/Bubble_Namespace/Scripts/BubbleBehaviour.cs
@@ -1,71 +1,59 @@
-using System;
-using DataStructures.Variables;
+using System.Collections;
+using System.Collections.Generic;
+using Leap.Unity;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Vector3 = UnityEngine.Vector3;
 
-namespace Features.Bubble_Namespace.Scripts
+public class BubbleBehaviour : MonoBehaviour
 {
-    public class BubbleBehaviour : MonoBehaviour
+    [SerializeField] private string correctInfoType; //will be set before the game starts via character view
+    private const float BUBBLE_SCALING = 0.03f;
+    public ParticleSystem bubblePop;
+    private Vector3 _scaleChange;
+    private int _hit = 0;
+    public bool adBlockerEnabled;
+
+    private void Start()
     {
-        [SerializeField] private string correctInfoType; //will be set before the game starts via character view
-        [SerializeField] private ParticleSystem bubblePop;
-        [SerializeField] private Animator [] activeCharacters;
-        [SerializeField] private BoolVariable bubbleIsPopped; 
-        public bool adBlockerEnabled;
-        private const float BUBBLE_SCALING = 0.03f;
-        private Vector3 _scaleChange;
-        private int _hit = 0;
-
-        private void Awake()
-        {
-            
-            _scaleChange = new Vector3(BUBBLE_SCALING, BUBBLE_SCALING, BUBBLE_SCALING);
-        }
-
-        private void Start()
-        {
-            bubbleIsPopped.Set(false);
-        }
-
-        private void OnCollisionEnter(Collision collision)
-        {
-            switch (collision.gameObject.tag)
-            {
-                case "InfoObject": 
-                    HitInfoItem(collision.gameObject);
-                    break; ;
-                case "MinimizeBubble":
-                    HitMinimizeBubble(collision.gameObject);
-                    break;
-            }
-        }
-        private void HitInfoItem(GameObject infoItem){
-            if (!infoItem.transform.name.Contains(correctInfoType))
-            {
-                _hit += 1;
-                transform.localScale += _scaleChange;
-            }
-            if(_hit == 3)
-            {
-                //Debug.Log("bubble popped in bubblebehaviour: " + bubbleIsPopped.Get());
-                bubbleIsPopped.Set(true);
-                Destroy(this.gameObject);
-                
-                bubblePop.Play();
-            }
-            infoItem.SetActive(false);
-        }
+        _scaleChange = new Vector3(BUBBLE_SCALING, BUBBLE_SCALING, BUBBLE_SCALING);
+    }
     
-        //TODO: call in HandBehaviour.cs
-        public void HitMinimizeBubble(GameObject minimizeItem)
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
         {
-            if (_hit >= 1 && _hit < 3)
-            {
-                _hit -= 1;
-                transform.localScale -= _scaleChange;
-            }
-            minimizeItem.GetComponent<SphereCollider>().enabled = false;
-            minimizeItem.SetActive(false);
+            case "InfoObject": 
+                HitInfoItem(collision.gameObject);
+                break; ;
+            case "MinimizeBubble":
+                HitMinimizeBubble(collision.gameObject);
+                break;
         }
+    }
+    private void HitInfoItem(GameObject infoItem){
+        if (!infoItem.transform.name.Contains(correctInfoType))
+        {
+            _hit += 1;
+            transform.localScale += _scaleChange;
+        }
+        if(_hit == 3)
+        {
+            Destroy(this.gameObject);
+            bubblePop.Play();
+        }
+        infoItem.SetActive(false);
+    }
+    
+    //TODO: call in HandBehaviour.cs
+    public void HitMinimizeBubble(GameObject minimizeItem)
+    {
+        if (_hit >= 1 && _hit < 3)
+        {
+            _hit -= 1;
+            transform.localScale -= _scaleChange;
+        }
+        minimizeItem.GetComponent<SphereCollider>().enabled = false;
+        minimizeItem.SetActive(false);
     }
 }
--- BubbleUp/Assets/Utilities/Variables/ColorVariable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace DataStructures.Variables
-{
-    [CreateAssetMenu(fileName = "NewColorVariable", menuName = "Utils/Variables/ColorVariable")]
-    public class ColorVariable : AbstractVariable<Color>
-    {
-    }
-}
