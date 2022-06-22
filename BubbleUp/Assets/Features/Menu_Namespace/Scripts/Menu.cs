76f0c78ac00ad5740398e6138ee30d9ee334f05f
Quynh Vi Trinh <quuynh.vi@gmail.com>
2022-06-22 20:14:25 +0200

    Added Pause Menu
    Added Pause menu with animations and transition

--- BubbleUp/Assets/Features/Menu_Namespace/Scripts/Menu.cs
+++ BubbleUp/Assets/Features/Menu_Namespace/Scripts/Menu.cs
@@ -25,7 +25,7 @@ public class Menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (GameIsPaused)
             {
