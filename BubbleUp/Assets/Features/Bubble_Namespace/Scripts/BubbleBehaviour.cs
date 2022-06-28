fe4529bf35af9fd3fabcfa77a04cb32f068d8b6c
Shiro Skrattberg <hikari.hanakimi@gmail.com>
2022-06-28 21:39:36 +0200

    Fix hand scene after merge

--- BubbleUp/Assets/Features/Bubble_Namespace/Scripts/BubbleBehaviour.cs
+++ BubbleUp/Assets/Features/Bubble_Namespace/Scripts/BubbleBehaviour.cs
@@ -20,24 +20,7 @@ namespace Features.Bubble_Namespace.Scripts
             bubbleIsPopped.Set(false);
             bubblePop.Stop();
         }
-        
-        private void OnCollisionEnter(Collision collision)
-        {
-            [SerializeField] private string correctInfoType; //will be set before the game starts via character view
-            [SerializeField] private BoolVariable bubbleIsPopped; 
-            private const float BUBBLE_SCALING = 0.03f;
-            public ParticleSystem bubblePop;
-            private Vector3 _scaleChange;
-            private int _hit = 0;
-            public bool adBlockerEnabled;
-        }
 
-        private void Start()
-        {
-            _scaleChange = new Vector3(BUBBLE_SCALING, BUBBLE_SCALING, BUBBLE_SCALING);
-            bubbleIsPopped.Set(false);
-        }
-    
         private void OnCollisionEnter(Collision other)
         {
             switch (other.gameObject.tag)
