8b0b5ba044dc67cb1856189ddc39124487a1b6b7
Shiro Skrattberg <hikari.hanakimi@gmail.com>
2022-06-29 21:45:25 +0200

    Fix errors from merge to branch

--- BubbleUp/Assets/Features/Menu_Namespace/Scripts/LoadCharacter.cs
+++ BubbleUp/Assets/Features/Menu_Namespace/Scripts/LoadCharacter.cs
@@ -12,5 +12,6 @@ public class LoadCharacter : MonoBehaviour
         int selectedCharacter = PlayerPrefs.GetInt("selectedCharacter");
         GameObject prefab = characterPrefabs[selectedCharacter];
         GameObject clone = Instantiate(prefab, spawnPoint.position, Quaternion.identity);
+        clone.SetActive(true);
     }
 }
