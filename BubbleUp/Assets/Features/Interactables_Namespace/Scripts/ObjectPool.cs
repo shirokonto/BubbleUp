750fec597561dcf2ff4355b949b04754ad95f84a
Shiro Skrattberg <hikari.hanakimi@gmail.com>
2022-07-02 18:31:16 +0200

    Add new approach to object pooling

---
+++ BubbleUp/Assets/Features/Interactables_Namespace/Scripts/ObjectPool.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Features.Interactables_Namespace.Scripts
+{
+    public class ObjectPool : MonoBehaviour
+    {
+        //use of queue to store reference to game object
+        private Dictionary<string, Queue<GameObject>> _itemPool;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _itemPool = new Dictionary<string, Queue<GameObject>>();
+        }
+
+        public void ReturnItemFromPool(GameObject item)
+        {
+            if (_itemPool.TryGetValue(item.name, out Queue<GameObject> itemList))
+            {
+                itemList.Enqueue(item);
+            }
+            else
+            {
+                Queue<GameObject> newItemQueue = new Queue<GameObject>();
+                newItemQueue.Enqueue(item);
+                _itemPool.Add(item.name, newItemQueue);
+            }
+        }
+
+        public GameObject GetItem(GameObject item){
+            if (_itemPool.TryGetValue(gameObject.name, out Queue<GameObject> itemList))
+            {
+                if (itemList.Count == 0)
+                {
+                    return CreateNewItem(item);
+                }
+                else
+                {
+                    GameObject pooledItem = itemList.Dequeue();
+                    pooledItem.SetActive(true);
+                    return pooledItem;
+                }
+            }
+            else
+            {
+                return CreateNewItem(item);
+            }
+        }
+
+        private GameObject CreateNewItem(GameObject item)
+        {
+            GameObject newItem = Instantiate(item);
+            newItem.name = item.name;
+            return newItem;
+        }
+    }
+}
